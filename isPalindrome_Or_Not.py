1855bbd5c32150db45275e16900ea57936c3f737
iamsurajx <66207975+iamsurajx@users.noreply.github.com>
2020-06-28 17:00:40 +0530

    Is_Palindrome
    python function to find given string is a palindrome or not a palindrome ::

---
+++ isPalindrome_Or_Not.py
@@ -0,0 +1,17 @@
+# string=input("Enter Name Here ::")
+# rev=string[::-1]
+# if string==rev:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+# function for palindrome
+def palin_fun(rev):
+    string = input("Enter Name Here ::")
+    rev = string[::-1]
+    if string == rev:
+        print("Palindrome")
+    else:
+        print("Not Palindrome")
+
+
